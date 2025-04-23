e6f1e31ac53eccdf3b948c019cc9646a57a2fe99
Ram <104311709+RamGemes@users.noreply.github.com>
2025-04-23 19:57:47 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<div style="text-align: center;">
+<img src="brand.png"  alt="Withered Hearts Logo">
+<h2>
+<b>A mod that only renders the withering hearts if they will wither away</b>
+</h2>
+<br>
+<a href="">
+<img alt="modrinth" height="56" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/cozy/available/modrinth_vector.svg">
+</a>
+<a href="https://github.com/Ramixin/Withered-Hearts">
+<img alt="github" height="56" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/cozy/available/github_vector.svg">
+</a>
+<br>
+<img alt="fabric" height="40" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/compact/supported/fabric_vector.svg">
+<img alt="no forge" height="40" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/compact/unsupported/forge_vector.svg">
+
+</div>
+
+# Does this support resource packs?
+
+---
+Yes! Long story short, Withered Hearts uses the library Mixson to automatically generate textures so that it can match
+any texture from any resource pack.
+
+## License
+
+---
+This project is under an MIT license.
---
+++ src/client/java/net/ramixin/witherhearts/client/mixins/InGameHudMixin.java
@@ -0,0 +1,52 @@
+package net.ramixin.witherhearts.client.mixins;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.ramixin.witherhearts.client.WitherHeartsClient;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Debug(export = true)
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+    @WrapOperation(method = "renderHealthBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud$HeartType;fromPlayerState(Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/client/gui/hud/InGameHud$HeartType;"))
+    private InGameHud.HeartType preventWitherHeartRenderMode(PlayerEntity player, Operation<InGameHud.HeartType> original, @Share("witheredHeartCount") LocalIntRef witheredHeartCount) {
+        InGameHud.HeartType type = original.call(player);
+        StatusEffectInstance effect = player.getStatusEffect(StatusEffects.WITHER);
+        if(effect == null) witheredHeartCount.set(0);
+        else witheredHeartCount.set(effect.getDuration() / (40 >> effect.getAmplifier()));
+        if(type == InGameHud.HeartType.WITHERED) return InGameHud.HeartType.NORMAL;
+        return type;
+    }
+
+    @WrapOperation(method = "renderHealthBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawHeart(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/gui/hud/InGameHud$HeartType;IIZZZ)V"))
+    private void preventWitherHeartIfRemaining(InGameHud instance, DrawContext context, InGameHud.HeartType type, int x, int y, boolean hardcore, boolean blinking, boolean half, Operation<Void> original, @Share("witheredHeartCount") LocalIntRef witheredHeartCount) {
+        InGameHud.HeartType changedType;
+        int left = witheredHeartCount.get();
+        boolean renderHalf = false;
+        if(type == InGameHud.HeartType.CONTAINER) changedType = type;
+        else if(left == 1) {
+            changedType = type;
+            renderHalf = true;
+            witheredHeartCount.set(0);
+        }
+        else if(left <= 0) changedType = type;
+        else {
+            changedType = InGameHud.HeartType.WITHERED;
+            witheredHeartCount.set(left - (half ? 1 : 2));
+        }
+        original.call(instance, context, changedType, x, y, hardcore, blinking, half);
+        if(renderHalf) context.drawGuiTexture(RenderLayer::getGuiTextured, WitherHeartsClient.generatedId(InGameHud.HeartType.WITHERED.getTexture(hardcore, true, blinking).getPath()), x, y, 9, 9);
+    }
+
+}
